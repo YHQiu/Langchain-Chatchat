1989658e7bcb65f1d275379f56a6bfb1a3b4eb1e
qiuhongyu <qiuhongyu@taoliweilai.com>
2023-10-05 15:18:17 +0800

    refact project structure

--- fmchain/__init__.py
+++ fmchain/__init__.py
@@ -1,2 +1,19 @@
 # Define the package name
 __package_name__ = "fmchain"
+from . import chains
+from . import common
+from . import configs
+from . import docs
+from . import document_loaders
+from . import embeddings
+from . import image
+from . import img
+from . import knowledge_base
+from . import server
+from . import tests
+from . import text_splitter
+from . import webui_pages
+from . import init_database
+from . import release
+from . import startup
+from . import webui
